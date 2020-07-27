daec0e5472f54ab4e3130fb7a492998755347db3
thirdratecyberpunk <blackbul@aston.ac.uk>
2020-07-27 23:33:33 +0100

    Improved graph generation with better filenames

--- gridworld/main.py
+++ gridworld/main.py
@@ -23,6 +23,8 @@ import torch
 import numpy as np
 import random
 
+import datetime
+
 def play(environment, agent, trials=500, max_steps_per_episode=1000, learn=False, update=False, target_update = 10):
     """runs a number of episodes of the given environment"""
     reward_per_episode = []
@@ -89,17 +91,17 @@ def main():
     
     args = parser.parse_args()
     
-    # setting device to send tensors to 
-    # TODO: get this working so agents run on GPU
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print (device)
+    # setting date/time for chart output
+    d = datetime.datetime.now()
     
     random_all_rewards_per_episode = []
     q_all_rewards_per_episode = []
+    sarsa_all_rewards_per_episode = []
     dq_all_rewards_per_episode = []
     
     random_all_steps_per_episode = [] 
     q_all_steps_per_episode = [] 
+    sarsa_all_steps_per_episode = []
     dq_all_steps_per_episode = [] 
     
     # running experiments and plotting results on a graph
@@ -117,24 +119,24 @@ def main():
         
         # running experiments
         random_reward_per_episode, random_total_steps = play(environment, random_agent, trials = args.trials, max_steps_per_episode= args.max_steps_per_episode)
-        q_learning_reward_per_episode, q_learning_total_steps = play(environment, q_learning_agent, trials = args.trials,
-                                                                     max_steps_per_episode= args.max_steps_per_episode, learn = True)
-        #    sarsa_learning_reward_per_episode, sarsa_total_steps = play(environment, sarsa_agent, trials = 1000, learn = True)
-        deep_q_learning_reward_per_episode, deep_q_learning_total_steps = play(environment, deep_q_learning_agent, trials = args.trials,
-                                                                               max_steps_per_episode= args.max_steps_per_episode, 
-                                                                               learn = True, update=True)
+#        q_learning_reward_per_episode, q_learning_total_steps = play(environment, q_learning_agent, trials = args.trials,
+#                                                                     max_steps_per_episode= args.max_steps_per_episode, learn = True)
+        sarsa_learning_reward_per_episode, sarsa_total_steps = play(environment, sarsa_agent, trials =args.trials, learn = True)
+#        deep_q_learning_reward_per_episode, deep_q_learning_total_steps = play(environment, deep_q_learning_agent, trials = args.trials,
+#                                                                               max_steps_per_episode= args.max_steps_per_episode, 
+#                                                                               learn = True, update=True)
         
         random_all_rewards_per_episode.append(random_reward_per_episode)
         random_all_steps_per_episode.append(random_total_steps)
         
-        q_all_rewards_per_episode.append(q_learning_reward_per_episode)
-        q_all_steps_per_episode.append(q_learning_total_steps)
+#        q_all_rewards_per_episode.append(q_learning_reward_per_episode)
+#        q_all_steps_per_episode.append(q_learning_total_steps)
         
-#        all_rewards_per_episode.append(sarsa_learning_reward_per_episode)
-#        all_steps_per_episode.append(sarsa_total_steps)
+        sarsa_all_rewards_per_episode.append(sarsa_learning_reward_per_episode)
+        sarsa_all_steps_per_episode.append(sarsa_total_steps)
         
-        dq_all_rewards_per_episode.append(deep_q_learning_reward_per_episode)
-        dq_all_steps_per_episode.append(deep_q_learning_total_steps)
+#        dq_all_rewards_per_episode.append(deep_q_learning_reward_per_episode)
+#        dq_all_steps_per_episode.append(deep_q_learning_total_steps)
         
         # diagram displaying loss over time
         fig = plt.figure()
@@ -142,12 +144,12 @@ def main():
         plt.xlabel("Trial")
         plt.ylabel("Loss")
         plt.plot(random_reward_per_episode, label="Random")
-        plt.plot(q_learning_reward_per_episode, label="Q-Learning")
-    #    plt.plot(sarsa_learning_reward_per_episode, label="SARSA")
-        plt.plot(deep_q_learning_reward_per_episode, label="Deep Q-Learning")
+#        plt.plot(q_learning_reward_per_episode, label="Q-Learning")
+        plt.plot(sarsa_learning_reward_per_episode, label="SARSA")
+#        plt.plot(deep_q_learning_reward_per_episode, label="Deep Q-Learning")
         plt.legend(loc="lower left")
         plt.show()
-        fig.savefig(f"environment_{environment.env_title}_seed_{seed}_loss_results.png")
+        fig.savefig(f"{args.output}/environment_{environment.env_title}_seed_{seed}_loss_results_{d:%Y-%m-%d_%H-%M-%S}.png")
         
         # diagram displaying number of steps taken over time
         fig = plt.figure()
@@ -155,20 +157,22 @@ def main():
         plt.xlabel("Trial")
         plt.ylabel("Number of steps taken")
         plt.plot(random_total_steps, label="Random")
-        plt.plot(q_learning_total_steps, label="Q-Learning")
-    #    plt.plot(sarsa_learning_reward_total_steps, label="SARSA")
-        plt.plot(deep_q_learning_total_steps, label="Deep Q-Learning")
+#        plt.plot(q_learning_total_steps, label="Q-Learning")
+        plt.plot(sarsa_total_steps, label="SARSA")
+#        plt.plot(deep_q_learning_total_steps, label="Deep Q-Learning")
         plt.legend(loc="lower left")
         plt.show()
-        fig.savefig(f"environment_{environment.env_title}_seed_{seed}_steps_taken_results.png")
+        fig.savefig(f"{args.output}/environment_{environment.env_title}_seed_{seed}_steps_taken_results_{d:%Y-%m-%d_%H-%M-%S}.png")
     
     # generating average performance over seeds
     average_random_rewards = generate_average_list_from_list_of_lists(random_all_rewards_per_episode)
     average_random_steps = generate_average_list_from_list_of_lists(random_all_steps_per_episode)
-    average_q_learning_rewards = generate_average_list_from_list_of_lists(q_all_rewards_per_episode)
-    average_q_learning_steps = generate_average_list_from_list_of_lists(q_all_steps_per_episode)
-    average_dq_learning_rewards = generate_average_list_from_list_of_lists(dq_all_rewards_per_episode)
-    average_dq_learning_steps = generate_average_list_from_list_of_lists(dq_all_steps_per_episode)
+#    average_q_learning_rewards = generate_average_list_from_list_of_lists(q_all_rewards_per_episode)
+#    average_q_learning_steps = generate_average_list_from_list_of_lists(q_all_steps_per_episode)
+    average_sarsa_learning_rewards = generate_average_list_from_list_of_lists(sarsa_all_rewards_per_episode)
+    average_sarsa_steps = generate_average_list_from_list_of_lists(sarsa_all_steps_per_episode)
+#    average_dq_learning_rewards = generate_average_list_from_list_of_lists(dq_all_rewards_per_episode)
+#    average_dq_learning_steps = generate_average_list_from_list_of_lists(dq_all_steps_per_episode)
     
     # diagram displaying seed average reward over time
     fig = plt.figure()
@@ -176,25 +180,25 @@ def main():
     plt.xlabel("Trial")
     plt.ylabel("Loss")
     plt.plot(average_random_rewards, label="Random")
-    plt.plot(average_q_learning_rewards, label="Q-Learning")
-    #    plt.plot(sarsa_learning_reward_per_episode, label="SARSA")
-    plt.plot(average_dq_learning_rewards, label="Deep Q-Learning")
+#    plt.plot(average_q_learning_rewards, label="Q-Learning")
+    plt.plot(average_sarsa_learning_rewards, label="SARSA")
+#    plt.plot(average_dq_learning_rewards, label="Deep Q-Learning")
     plt.legend(loc="lower left")
     plt.show()
-    fig.savefig(f"environment_{environment.env_title}_average_loss_results.png")
+    fig.savefig(f"{args.output}/environment_{environment.env_title}_average_loss_results_{d:%Y-%m-%d_%H-%M-%S}.png")
     
     # diagram displaying seed average steps taken over time
     fig = plt.figure()
-    plt.title(f"{environment.env_title} steps taken per trial for average of seeds {args.seeds}")
+    plt.title(f"{args.output}/{environment.env_title} steps taken per trial for average of seeds {args.seeds}")
     plt.xlabel("Trial")
     plt.ylabel("Number of steps taken")
     plt.plot(average_random_steps, label="Random")
-    plt.plot(average_q_learning_steps, label="Q-Learning")
-    #    plt.plot(sarsa_learning_reward_per_episode, label="SARSA")
-    plt.plot(average_dq_learning_steps, label="Deep Q-Learning")
+#    plt.plot(average_q_learning_steps, label="Q-Learning")
+    plt.plot(average_sarsa_steps, label="SARSA")
+#    plt.plot(average_dq_learning_steps, label="Deep Q-Learning")
     plt.legend(loc="lower left")
     plt.show()
-    fig.savefig(f"environment_{environment.env_title}_average_steps_results.png")
+    fig.savefig(f"{args.output}/environment_{environment.env_title}_average_steps_results_{d:%Y-%m-%d_%H-%M-%S}.png")
     
 if __name__ == "__main__":
     main()
